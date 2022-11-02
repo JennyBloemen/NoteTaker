bb02f7e8c661e3ae20fac0cc5a3a9d894c9732b0
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-02 14:22:28 -0600

    Removed  vairables not needed.

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const path = require('path');
-// const fs = require('fs');  //file system that can read, write and modify files that I pass
+const express = require("express");
+// const path = require("path");
+
+// const uuid = require("./helpers/uuid");
+const fs = require("fs"); //file system that can read, write and modify files that I pass
 // const noteData = require('./db/db.json');
-const apiRoutes = require('./routes/apiroutes');
-const htmlRoutes = require('./routes/htmlroutes');
-const PORT = process.env.PORT || 3001; //pulled from lesson 25 
+const apiRoutes = require("./routes/api");
+const htmlRoutes = require("./routes/htmlroutes");
+const PORT = process.env.PORT || 3001; //pulled from lesson 25
 
 const app = express();
 
 app.use(express.json()); //required to make a call
-app.use(express.urlencoded({extended: true})); //required for form data
-app.use(express.static('public')); //this allows the client side to use anything in the public folder without having and end point defined
+app.use(express.urlencoded({ extended: true })); //required for form data
+app.use(express.static("public")); //this allows the client side to use anything in the public folder without having and end point defined
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
