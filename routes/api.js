85b8a383a1c5db681441007a0dd8725a67fef3e6
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-11-09 15:09:03 -0700

    Cleaned up old notes

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const fs = require("fs"); //file system
-const path = require("path");
-
-const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 const util = require("util");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
+//route to get the notes 
 router.get("/notes", (req, res) => {
   fs.readFile("db/db.json", "utf-8", (err, data) => {
     return err ? console.log(err) : res.json(JSON.parse(data));
   });
 });
-
+//route to save new notes
 router.post("/notes", (req, res) => {
   const { title, text } = req.body;
 
@@ -38,24 +38,29 @@ router.post("/notes", (req, res) => {
     });
   });
 });
-
+//route to delete notes, does not work
 router.delete("/api/notes/:id", (req, res) => {
+  console.log("hit");
   let deleteId = req.params.id;
+  fs.readFile("./db/db.json", (err, data) => {
+    let dbInfo = JSON.parse(data); //handling data read from ./db/db.json
 
-  util
-    .promisify(fs.readFile("./db/db.json"))
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-      const result = json.filter((note) => note.id !== deleteId);
-      console.log(result);
-      writetoFile("./db/db.json", result);
-      res.json("Note has been deleted 🗑️");
+    console.log(dbInfo, deleteId);
+    dbInfo = dbInfo.filter((n) => {
+      //this is a type of for loop
+      return n.id != deleteId;
     });
+    console.log(dbInfo);
+  });
+  const dbNotes = JSON.stringify(dbInfo);
+  fs.writeFile("./db/db.json", dbNotes, (err) => {
+    if (err) {
+      console.log("Error");
+    } else {
+      console.log("Note added");
+      res.sendFile(path.join(__dirname, "../", "/db/db.json"));
+    }
+  });
 });
 
 module.exports = router;
-
-// const deleteInfo = JSON.parse(data); // jsonify it just as above
-// in the array of objects returned, get the one matching your id from the request
-// remove it from the array
-// save the array back in db.json just as above
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require("express");
-// const path = require("path");
 
-// const uuid = require("./helpers/uuid");
-const fs = require("fs"); //file system that can read, write and modify files that I pass
-// const noteData = require('./db/db.json');
+const fs = require("fs");
+
 const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/htmlroutes");
-const PORT = process.env.PORT || 3001; //pulled from lesson 25
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json()); //required to make a call
